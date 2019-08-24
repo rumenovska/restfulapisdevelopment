b06c4fea77c91b5392f966a0db5e73b9068a25fc
Marija Rumenovska <marija.rumenovska@hotmail.com>
2019-08-24 12:17:27 +0200

    Phonebook Class 6

---
+++ PhonebookApp/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using DataModels;
+using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<DtoUser> _userRepo;
+      
+        public UserService(IRepository<DtoUser> userRepo)
+        {
+            _userRepo = userRepo;
+           
+        }
+
+        public UserModel Authenticate(string username, string password)
+        {
+
+            throw new NotImplementedException();
+
+
+        }
+
+        public void Register(RegisterModel registerModel)
+        {
+            
+        }
+    }
+}
